7be1972ef85ef175b2449c115e4a8f232f627490
LY <514511987@qq.com>
2018-03-25 03:28:05 +0800

    readme

--- Algorithm/test.js
+++ Algorithm/test.js
@@ -1,31 +1,4 @@
-const toChiesesNum = n => {
-	const chiesesNum = ['零', '一', '二', '三', '四', '五', '六', '七', '八', '九']
-	const section = ['', '十', '百', '千']
-	let bigNum = parseInt(n / 10000)
-	let litNum = n % 10000
-	const toChiesesSection = num => {
-		const arr = Array.from(num.toString())
-		let newNum = ''
-		let count = 0
-		for (let i = arr.length - 1; i >= 0; i--) {
-			if (arr[i] !== '0') {
-				newNum = `${chiesesNum[arr[i]]}${section[count]}${newNum}`
-			} else {
-				if (i === arr.length - 1) {
-					newNum = ''
-				} else {
-					if (arr[i - 1] !== '0' && arr[arr.length - 1] !== '0') {
-						newNum = `${chiesesNum[arr[i]]}${newNum}`
-					}
-				}
-			}
-			count++
-		}
-		return newNum
-	}
-	if (bigNum > 0) {
-		return `${toChiesesSection(bigNum)}万${litNum > 0 && litNum < 1000 ? '零' : ''}${toChiesesSection(litNum)}`
-	} else {
-		return toChiesesSection(litNum)
-	}
-}
+let l = [0, 1, [1, [2, [3, [4, [5, [6]]]]]]];
+const flatten = (arr) => arr.reduce((acc, val) => acc.concat(Array.isArray(val) ? flatten(val) : val), [])
+console.log(flatten(l))
+
